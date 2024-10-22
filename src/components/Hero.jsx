6499c8a8ea92c3fad6cc44a5f17c3283dbcdc78e
Mohammed Mudabbir Pasha <116947092+mudabbir525@users.noreply.github.com>
2024-10-23 00:55:02 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
-import { HERO_CONTENT } from "../constants/index"; // Ensure this content reflects your company's services
-import Profile from '../assets/Mylogo.png'; // You may want to update this with your company logo or an appropriate image
+import { HERO_CONTENT } from "../constants/index"; 
+import Profile from '../assets/Mylogo.png'; // change it
 import { motion } from "framer-motion";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 const container = (delay) => ({
     hidden: { x: -100, opacity: 0 },
@@ -39,6 +40,7 @@ const Hero = ({ setCurrentComponent }) =>{
                             className="my-2 max-w-xl py-6 font-light tracking-tight">
                             {HERO_CONTENT} {/* Update this content in constants/index.js */}
                         </motion.p>
+                        <Link to="/contact">
                         <motion.button
                             onClick={() => setCurrentComponent('contact')}  // Switch to contact page
                             variants={container(1)}
@@ -49,6 +51,7 @@ const Hero = ({ setCurrentComponent }) =>{
                         >
                             Get Started
                         </motion.button>
+                            </Link>
 
                     </div>
                 </div>
